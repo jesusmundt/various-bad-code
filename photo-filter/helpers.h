af782d7b852be989e9af7cff5858c9b3a8292146
Jesús Medina <110201704+jesusmundt@users.noreply.github.com>
2024-02-05 10:27:04 +0000

    Mon, Feb 5, 2024, 10:27 AM +00:00

--- photo-filter/helpers.h
+++ photo-filter/helpers.h
@@ -7,8 +7,8 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width]);
 void reflect(int height, int width, RGBTRIPLE image[height][width]);
 
 // Detect edges
-void edges(int height, int width, RGBTRIPLE image[height][width]);
+void edges(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE imagecopy[height][width]);
 
 // Blur image
-void blur(int height, int width, RGBTRIPLE image[height][width]);
+void blur(int height, int width, RGBTRIPLE image[height][width], RGBTRIPLE imagecopy[height][width]);
 
