337f5a8b1140cfee66b7bc00f461efe04dfcebe6
Jesús Medina <110201704+jesusmundt@users.noreply.github.com>
2024-02-05 20:32:52 +0000

    Mon, Feb 5, 2024, 8:32 PM +00:00

--- shooting/helpers.c
+++ shooting/helpers.c
@@ -56,7 +56,7 @@ rgbvalues getBackground(int height, int width, RGBTRIPLE image[height][width])
 
             if ((image[k][l].rgbtBlue + image[k][l].rgbtGreen + image[k][l].rgbtRed) / 3 < 20 || (image[k][l].rgbtBlue + image[k][l].rgbtGreen + image[k][l].rgbtRed) / 3 > 200 )
             {
-                for (int m = k - 5; m < k + 15; m++)
+                for (int m = k - 5; m < k + 10; m++)
                 {
                     for (int n = l; n < l + 15; n++)
                     {
