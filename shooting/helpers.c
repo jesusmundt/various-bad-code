4f5a7f8f310008ea9223391b8e2fd93f3d6829b6
Jesús Medina <110201704+jesusmundt@users.noreply.github.com>
2024-02-05 12:28:23 +0000

    Mon, Feb 5, 2024, 12:28 PM +00:00

--- shooting/helpers.c
+++ shooting/helpers.c
@@ -19,7 +19,20 @@ rgbvalues getBackground(int height, int width, RGBTRIPLE image[height][width])
     rgbvalues average.green = 0;
     rgbvalues average.red = 0;
 
-    
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+
+            average = round((image[i][j].rgbtBlue + image[i][j].rgbtGreen + image[i][j].rgbtRed) / 3.0);
+            image[i][j].rgbtBlue = average;
+            image[i][j].rgbtGreen = average;
+            image[i][j].rgbtRed = average;
+
+        }
+    }
+    return;
+
 }
 
 // Convert image to grayscale
