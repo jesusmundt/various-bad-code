f56b3cea6e3234032343bd0fd3ba1b7831d7a6d6
Jesús Medina <110201704+jesusmundt@users.noreply.github.com>
2024-02-05 19:24:10 +0000

    Mon, Feb 5, 2024, 7:24 PM +00:00

--- shooting/helpers.c
+++ shooting/helpers.c
@@ -57,7 +57,9 @@ rgbvalues getBackground(int height, int width, RGBTRIPLE image[height][width])
                     sum.green = sum.green + image[m][n].rgbtGreen;
                     sum.red = sum.red + image[m][n].rgbtRed;
 
-                    average.blue = sum.blue / 
+                    average.blue = sum.blue / 400;
+                    average.green = sum.green / 400;
+                    average.red = sum.blue / 400;
 
                 }
 
